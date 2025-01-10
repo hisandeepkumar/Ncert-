2c72afdda608255bf10b6599973e493984cc6d8e
Sandeep  Yadav <84236760+hisandeepkumar@users.noreply.github.com>
2025-01-10 20:03:35 +0530

    Update matrix-effect.js

--- matrix-effect.js
+++ matrix-effect.js
@@ -25,4 +25,4 @@ function createMatrixLine() {
 }
 
 // Reduced density by increasing the interval
-setInterval(createMatrixLine, 200); // Every 200ms instead of 50ms
+setInterval(createMatrixLine, 500); // Every 200ms instead of 50ms
